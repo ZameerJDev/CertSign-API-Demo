c323c124e7d97855b08b0d98776d8571e1df72da
Zameer Jiwani <zameer.jiwani@gmail.com>
2017-09-23 21:16:28 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Generate Mobileconfig</title>
+</head>
+<body>
+<form action="verify.php" method="GET">
+	App Name: <input type="text" name="name">
+</form>
+</body>
+</html>
