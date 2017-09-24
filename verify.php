4bda9a46901a5242cdca5e1cb77f19f105464aa2
Zameer Jiwani <zameer.jiwani@gmail.com>
2017-09-24 11:57:59 -0500

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,4 +1,5 @@
 <?php
     header('Content-type: application/x-apple-aspen-config; charset=utf-8');
-
-echo file_get_contents('http://zameer.us/certsign/api.php?url=https://Link/To/build.php?name='.$_GET['name']);
+//GET API KEY FROM http://zameer.us/certsign/createkey.php
+$apiKey="";
+echo file_get_contents('http://zameer.us/certsign/api.php?key='.$apiKey.'&url=https://Link/To/build.php?name='.$_GET['name']);
