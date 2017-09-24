bd45494d73c4bf230d3f442290ab6ec36c24fdaa
Zameer Jiwani <zameer.jiwani@gmail.com>
2017-09-23 21:12:43 -0500

    Update req.php

--- req.php
+++ req.php
@@ -1,4 +1,4 @@
 <?php
     header('Content-type: application/x-apple-aspen-config; charset=utf-8');
 
-echo file_get_contents('http://zameer.us/certsign/api.php?url=https://zameer.us/certsign/demo/build.php?name='.$_GET['name']);
+echo file_get_contents('http://zameer.us/certsign/api.php?url=https://Link/To/build.php?name='.$_GET['name']);
