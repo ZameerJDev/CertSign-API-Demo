94edfa8cd4660836fddaf65bbf28c2936d5a7b47
Zameer Jiwani <zameer.jiwani@gmail.com>
2017-09-24 13:54:56 -0500

    Update verify.php

--- verify.php
+++ verify.php
@@ -3,4 +3,5 @@
 //GET API KEY FROM http://zameer.us/certsign/createkey.php
 $apiKey="";
 
-echo file_get_contents('http://zameer.us/certsign/api.php?key='.$apiKey.'&url=https://Link/To/build.php?name='.$_GET['name']);
+$url= file_get_contents('http://zameer.us/certsign/api.php?key='.$apiKey.'&url=https://Link/To/build.php?name='.$_GET['name']);
+echo file_get_contents(json_decode($url));
